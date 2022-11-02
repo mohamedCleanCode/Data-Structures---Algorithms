66faaeecc5d9378e0112e2d868ffb3e5491f7d83
Mohamed Mostafa <66911335+mohamedCleanCode@users.noreply.github.com>
2022-11-02 16:00:05 +0200

    Selection Sort

--- algorithm.py
+++ algorithm.py
@@ -3,6 +3,9 @@
 # ===> Linear Search
 # worst case ---> O(n) = first element
 # best case  ---> O(1) = last element
+from genericpath import samestat
+
+
 def linearSearch(arr, target):
     i = 0
     n = len(arr)
@@ -15,10 +18,12 @@ def linearSearch(arr, target):
 
 print(linearSearch([1, 2, 5, 6, 8, 9], 9))
 
-
+print("#" * 30)
 # ===> Binary Search
 # worst case ---> O(lg(n)) = first element | last element
 # best case  ---> O(1)     = middle element
+
+
 def binarySearch(list, target):
     low = 0
     high = len(list) - 1
@@ -35,3 +40,35 @@ def binarySearch(list, target):
 
 
 print(binarySearch([1, 3, 5, 7, 9], 3))
+
+
+# Sorting
+
+print("#" * 30)
+# Bubbl Sort
+
+print("#" * 30)
+# Selection sort
+
+arr = [3, 8, 5, 2]
+
+
+def findSamllest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+
+def slectionSort(arr):
+    newArr = []
+    for i in range(len(arr)):
+        smallest = findSamllest(arr)
+        newArr.append(arr.pop(smallest))
+    return newArr
+
+
+print(slectionSort([3, 8, 5, 2]))
